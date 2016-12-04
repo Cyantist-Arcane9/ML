68330c6063c69c35c1a3656f2e9d2f7fdbb3e8e6
Arcane9 <arcane9@outlook.com>
2016-12-04 15:29:30 +0530

    naive bayes

---
+++ naiveBayes.py
@@ -0,0 +1,57 @@
+import os
+import io
+import numpy
+from pandas import DataFrame
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+
+# Read files from path recursively for training!
+def readFiles(path):
+    for root, dirnames, filenames in os.walk(path):
+        for filename in filenames:
+            path = os.path.join(root, filename)
+            inBody = False
+            lines = []
+            f = io.open(path, 'r', encoding='latin1')
+            for line in f:
+                if inBody:
+                    lines.append(line)
+                elif line == '\n':
+                    inBody = True
+            f.close()
+            message = '\n'.join(lines)
+            yield path, message
+
+# Creating data frame : spam / ham
+def dataFrameFromDirectory(path, classification):
+    rows = []
+    index = []
+    for filename, message in readFiles(path):
+        rows.append({'message': message, 'class': classification})
+        index.append(filename)
+    return DataFrame(rows, index=index)
+
+def nBayes():
+    # Creating pandas dataframe consisting : message & its Class : Spam/ham
+    data = DataFrame({'message': [], 'class': []})
+
+    data = data.append(dataFrameFromDirectory('./data/emails/spam', 'spam'))
+    data = data.append(dataFrameFromDirectory('./data/emails/ham', 'ham'))
+
+    data.head()
+
+    # Converting Message into its list of words
+    vectorizer = CountVectorizer()
+    counts = vectorizer.fit_transform(data['message'].values)
+
+    # Using MultinomialNB classifier
+    classifier = MultinomialNB()
+    targets = data['class'].values
+    classifier.fit(counts, targets)
+
+    # Testing out email to get result
+    examples = ['Demonitization Rocks!!!', "Hi Ashish, how about your thoughts on this?"]
+    example_counts = vectorizer.transform(examples)
+    predictions = classifier.predict(example_counts)
+    print(predictions)
+    print('')
